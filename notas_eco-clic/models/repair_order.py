d38aa0ba32bbdfec4ec734bb58c01a799c5f8dcb
Eco-Clic <tecnico@eco-clic.net>
2024-07-11 12:36:43 +0200

    Delete notas_eco-clic directory

--- notas_eco-clic/models/repair_order.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class Repair(models.Model):
-    _inherit = 'repair.order'
-
-    notas_internas = fields.Char(string="Notas")
--- notas_eco-clic/models/sale_order.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    notas_internas = fields.Char(string="Notas")
